2debd35abec278486abe19f452cbc061ed2b6a91
Anthymm <ells.anton04@gmail.com>
2024-10-28 10:01:14 +0100

    Added Database

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
+const dotenv = require('dotenv'),
+  { Client } = require('pg')
+
+dotenv.config()
+
+const client = new Client({
+  connectionString: process.env.PGURI
+})
+
+client.connect()
+
 const express = require('express'),
   path = require('path')
 
